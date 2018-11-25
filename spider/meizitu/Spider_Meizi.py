b2e0288466db27e6fc6ca7d3524e72bf52063a93
时海鑫 <18545156106@163.com>
2018-11-25 14:14:35 +0800

    meizitu

--- spider/meizitu/Spider_Meizi.py
+++ spider/meizitu/Spider_Meizi.py
@@ -1,10 +1,23 @@
 from utils.SpiderUtil import SpiderHtml
+from bs4 import BeautifulSoup as bs
+import requests
 
 
 def runSpider(url):
     html = SpiderHtml(url).getHtml()
-    print(html)
+    p_s = bs(html, 'html.parser').find_all('ul', class_='archives')
+    for i in p_s:
+        a_s = i.find_all('a')
+
+        print(len(a_s))
+
+
+def getImage(url):
+    html = SpiderHtml(url).getHtml()
+    link=bs(html, 'html.parser').find('figure').find('img')['src']
+    print(link)
 
 
 if __name__ == "__main__":
-    runSpider('https://www.mzitu.com/all')
+    # runSpider('https://www.mzitu.com/all')
+    getImage('https://www.mzitu.com/159605')
