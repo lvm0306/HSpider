8cc70d14c92a3067dce85f92257338a3ecc70903
lovesosoi <lovesosomeng@gmail.com>
2022-06-11 22:02:45 +0800

    0929 laji

--- pyqt5/pysider2_ui02.py
+++ pyqt5/pysider2_ui02.py
@@ -21,26 +21,26 @@ class Stats:
 
     def handleCalc(self):
         info = self.ui.textEdit.toPlainText()
-
-        salary_above_20k = ''
-        salary_below_20k = ''
-        for line in info.splitlines():
-            if not line.strip():
-                continue
-            parts = line.split(' ')
-
-            parts = [p for p in parts if p]
-            name,salary,age = parts
-            if int(salary) >= 20000:
-                salary_above_20k += name + '\n'
-            else:
-                salary_below_20k += name + '\n'
-
-        QMessageBox.about(self.ui,
-                    '统计结果',
-                    f'''薪资20000 以上的有：\n{salary_above_20k}
-                    \n薪资20000 以下的有：\n{salary_below_20k}'''
-                    )
+        os.system('ffmpeg -i '+info)
+        # salary_above_20k = ''
+        # salary_below_20k = ''
+        # for line in info.splitlines():
+        #     if not line.strip():
+        #         continue
+        #     parts = line.split(' ')
+        #
+        #     parts = [p for p in parts if p]
+        #     name,salary,age = parts
+        #     if int(salary) >= 20000:
+        #         salary_above_20k += name + '\n'
+        #     else:
+        #         salary_below_20k += name + '\n'
+        #
+        # QMessageBox.about(self.ui,
+        #             '统计结果',
+        #             f'''薪资20000 以上的有：\n{salary_above_20k}
+        #             \n薪资20000 以下的有：\n{salary_below_20k}'''
+        #             )
 
 app = QApplication([])
 stats = Stats()
--- pyqt5/pyspider2_ui_http.py
+++ pyqt5/pyspider2_ui_http.py
@@ -1,7 +1,12 @@
+import os
 from urllib import request
 
+import PySide2
 import requests
 from PySide2.QtWidgets import QApplication, QMessageBox
+dirname = os.path.dirname(PySide2.__file__)
+plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
 from PySide2.QtUiTools import QUiLoader
 
 class Stats:
